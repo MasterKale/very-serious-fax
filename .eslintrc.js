d63681e5ba7a7ae1ef85057d4682d33c4a5a8772
Matthew Miller <kale@iammiller.com>
2018-09-18 08:11:36 -0700

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: "airbnb-base/legacy",
+    globals: {
+      "AFRAME": true,
+    },
+    rules: {
+      "func-names": "off",
+      "comma-dangle": ["error", "always-multiline"],
+    }
+};
